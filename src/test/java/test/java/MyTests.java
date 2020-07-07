22920b87840eaf83f5eb7ed5de38699f2a48ca5c
Valentin Ionascu <valentin.ionascu@endava.com>
2020-07-07 16:55:20 +0300

    convert to mvn

---
+++ src/test/java/test/java/MyTests.java
@@ -0,0 +1,28 @@
+package test.java;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import com.taa2020.day1.Calculator;
+
+public class MyTests {
+
+    private static final Calculator calculator = new Calculator();
+    private static final Logger logger = LogManager.getLogger(MyTests.class);
+
+    public static void main(String[] args) {
+
+        logger.info(calculator.add2(1,2));
+
+    }
+
+    // @Test
+    // public void myFirstTest() {
+    //     System.out.println(calculator.add2(1,2));
+    //     assertEquals(4, calculator.add2(1,2));
+    // }
+
+}
